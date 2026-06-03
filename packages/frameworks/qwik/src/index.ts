4968df2e70e5078359f19ace33cd5085f1788aa2
Gabriel Grant <g@briel.ca>
2026-06-03 15:11:53 -0400

    refactor(qwik): remove runtime symbol registration

--- packages/frameworks/qwik/src/index.ts
+++ packages/frameworks/qwik/src/index.ts
@@ -1,8 +1,4 @@
-import { registerQwikRuntimeSymbols } from "./runtime-symbols"
-
 export { mergeProps } from "@zag-js/core"
 export { bindProps, splitProps, type SplitProps, type ZagProps } from "./bind-props"
 export * from "./machine"
 export * from "./normalize-props"
-
-registerQwikRuntimeSymbols()
