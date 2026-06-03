7a5bb61a1d0a2ea51cf1b608d950c2afab711b7f
Gabriel Grant <g@briel.ca>
2026-06-03 16:22:51 -0400

    test(qwik): simplify scaffolded vite config

--- examples/qwik-ts/vite.config.ts
+++ examples/qwik-ts/vite.config.ts
@@ -2,15 +2,6 @@ import { qwikVite } from "@qwik.dev/core/optimizer"
 import { qwikRouter } from "@qwik.dev/router/vite"
 import { defineConfig, type UserConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
-import pkg from "./package.json"
-
-type PkgDep = Record<string, string>
-const { dependencies = {}, devDependencies = {} } = pkg as any as {
-  dependencies: PkgDep
-  devDependencies: PkgDep
-  [key: string]: unknown
-}
-errorOnDuplicatesPkgDeps(devDependencies, dependencies)
 
 export default defineConfig(
   (): UserConfig => ({
@@ -30,15 +21,3 @@ export default defineConfig(
     },
   }),
 )
-
-function errorOnDuplicatesPkgDeps(devDependencies: PkgDep, dependencies: PkgDep) {
-  const qwikPkg = Object.keys(dependencies).filter((value) => value.startsWith("@qwik.dev/"))
-  if (qwikPkg.length > 0) {
-    throw new Error(`Move qwik packages ${qwikPkg.join(", ")} to devDependencies`)
-  }
-
-  const duplicateDeps = Object.keys(devDependencies).filter((dep) => dependencies[dep])
-  if (duplicateDeps.length > 0) {
-    throw new Error(`Move duplicated dependencies to devDependencies only: ${duplicateDeps.join(", ")}`)
-  }
-}
